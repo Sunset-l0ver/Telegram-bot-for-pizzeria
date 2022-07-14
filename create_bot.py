4ed0434936675337196324c36138b47fe289397f
Sunset-l0ver <vagr.karapetian@gmail.com>
2022-07-14 15:32:28 +0300

    8

--- create_bot.py
+++ create_bot.py
@@ -2,10 +2,13 @@ from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+
 BOT_TOKEN = "5367036604:AAHEZvXO7nOGZcysWgsOSF52MWaPndepAHk"
 ADMINS = ["1547396618", ]
 
+
 storage = MemoryStorage()
+
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot, storage=storage)
