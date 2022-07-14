632bfc55a6597d35e4d533178bf5e1241ccb6a30
Sunset-l0ver <vagr.karapetian@gmail.com>
2022-07-14 15:20:56 +0300

    8

--- Bot.py
+++ Bot.py
@@ -13,5 +13,6 @@ client.register_handlers_client(dp)
 admin.register_handlers_admin(dp)
 other.register_handlers_other(dp)
 
+
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
